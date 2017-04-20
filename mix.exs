df7b3041bdaba4e096075b77f6f9907214eee9b3
Paolo D'Incau <paolo.dincau@gmail.com>
2017-04-20 21:50:55 +0200

    initial commit

---
+++ mix.exs
@@ -0,0 +1,36 @@
+defmodule ExJenkins.Mixfile do
+  use Mix.Project
+
+  def project do
+    [app: :ex_jenkins,
+     version: "0.1.0",
+     elixir: "~> 1.4",
+     build_embedded: Mix.env == :prod,
+     start_permanent: Mix.env == :prod,
+     deps: deps()]
+  end
+
+  # Configuration for the OTP application
+  #
+  # Type "mix help compile.app" for more information
+  def application do
+    [extra_applications: [:logger],
+     mod: {ExJenkins, []},
+    applications: [:httpoison]]
+  end
+
+  # Dependencies can be Hex packages:
+  #
+  #   {:my_dep, "~> 0.3.0"}
+  #
+  # Or git/path repositories:
+  #
+  #   {:my_dep, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+  #
+  # Type "mix help deps" for more examples and options
+  defp deps do
+    [{:httpoison, "~> 0.10.0"},
+     {:poison, "~> 3.1"}
+    ]
+  end
+end
---
+++ test/ex_jenkins_test.exs
@@ -0,0 +1,8 @@
+defmodule ExJenkinsTest do
+  use ExUnit.Case
+  doctest ExJenkins
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
