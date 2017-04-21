34ea4f210f037db81c6b88cc7a2f48fafc79a585
Paolo D'Incau <paolo.dincau@gmail.com>
2017-04-21 22:25:59 +0200

    add folder endpoint

---
+++ lib/ex_jenkins/endpoints/folders.ex
@@ -0,0 +1,60 @@
+defmodule ExJenkins.Folders do
+
+  use HTTPoison.Base
+
+  alias HTTPoison.{Response, Error}
+  alias ExJenkins.Headers
+
+  def create(folder) do
+    request(:post, "createItem?name=" <> folder <> rest_of_url(), "", [{"Content-Type", "application/x-www-form-urlencoded"}], [])
+    |> handle_create_folder_response
+  end
+
+  def delete(folder) do
+    post("job/" <> folder <> "/doDelete", "")
+    |> handle_delete_folder_response
+  end
+
+  defp handle_create_folder_response(response) do
+    case response do
+      {:ok, %Response{status_code: 302}} ->
+        {:ok, :created}
+      {:ok, %Response{status_code: 400}} ->
+        {:error, :cannot_create}
+      error_response ->
+        handle_error(error_response)
+    end
+  end
+
+  defp handle_delete_folder_response(response) do
+    case response do
+      {:ok, %Response{status_code: 302}} ->
+        {:ok, :deleted}
+      error_response ->
+        handle_error(error_response)
+    end
+  end
+
+  defp handle_error(response) do
+    case response do
+      {:ok, %Response{status_code: 404}} ->
+        {:error, :not_found}
+      {:error, %Error{reason: _reason}} ->
+        {:error, :generic_error}
+    end
+  end
+
+  defp process_request_headers(headers) do
+    headers
+    |> Headers.add_authorization_header()
+    |> Headers.add_crumb_header()
+  end
+
+  defp process_url(endpoint) do
+    ExJenkins.base_url <> endpoint
+  end
+
+  defp rest_of_url do
+    "&mode=com.cloudbees.hudson.plugins.folder.Folder&from=&json=%7B%22name%22%3A%22FolderName%22%2C%22mode%22%3A%22com.cloudbees.hudson.plugins.folder.Folder%22%2C%22from%22%3A%22%22%2C%22Submit%22%3A%22OK%22%7D&Submit=OK"
+  end
+end
