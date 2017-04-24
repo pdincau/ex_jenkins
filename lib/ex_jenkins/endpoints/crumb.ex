887cd4d655c36b0c8f5a83c23f44b86e6f650dc6
Paolo D'Incau <paolo.dincau@gmail.com>
2017-04-24 18:43:55 +0200

    adding docs for crumb.ex

--- lib/ex_jenkins/endpoints/crumb.ex
+++ lib/ex_jenkins/endpoints/crumb.ex
@@ -1,10 +1,20 @@
 defmodule ExJenkins.Crumb do
 
+  @moduledoc """
+    This module provides functionalities to handle Jenkins crumb.
+  """
+
   use HTTPoison.Base
 
   alias HTTPoison.{Response, Error}
   alias ExJenkins.Headers
 
+  @doc """
+    Issue a Jenkins crumb.
+    ## Examples
+        iex> ExJenkins.Crumbs.issue
+        {:ok, {{:value, "yourcrumb"}, {:request_field, "jenksinrequestfield"}}}
+  """
   def issue do
     case get("crumbIssuer/api/json") do
       {:ok, %Response{status_code: 200, body: body}} ->
