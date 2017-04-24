b8a2e5476eacaf23a1dd58a322e3a95485447033
Paolo D'Incau <paolo.dincau@gmail.com>
2017-04-24 18:58:28 +0200

    add docs for jenkins.ex

--- lib/ex_jenkins/endpoints/jenkins.ex
+++ lib/ex_jenkins/endpoints/jenkins.ex
@@ -1,20 +1,49 @@
 defmodule ExJenkins.Jenkins do
 
+  @moduledoc """
+    This module provides functionalities to deal with Jenkins.
+  """
+
   use HTTPoison.Base
 
   alias HTTPoison.{Response, Error}
   alias ExJenkins.Headers
 
+  @doc """
+    Quiet down Jenkins.
+
+    ## Examples
+
+        iex> ExJenkins.Jenkins.quiet_down
+        {:ok, :quieted_down}
+  """
   def quiet_down do
     post("quietDown", "")
     |> handle_quiet_down_cancel_quiet_down_response(:quieted_down)
   end
 
+  @doc """
+    Cancel quiet down request to Jenkins.
+
+    ## Examples
+
+        iex> ExJenkins.Jenkins.cancel_quiet_down
+        {:ok, :cancel_quieted_down}
+  """
   def cancel_quiet_down do
     post("cancelQuietDown", "")
     |> handle_quiet_down_cancel_quiet_down_response(:cancel_quieted_down)
   end
 
+
+  @doc """
+    Restarts Jenkins.
+
+    ## Examples
+
+        iex> ExJenkins.Jenkins.restart
+        {:ok, :restart_command_issued}
+  """
   def restart(mode \\ :safe) do
     restart_endpoint(mode)
     |> post("")
