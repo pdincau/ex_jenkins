2105cffd98f258e99298b013e0b261f03b918314
Marc Salles <marc.salles@xing.com>
2022-01-18 13:38:27 +0100

    adding more tests for Headers.extract

--- test/helpers/headers_test.exs
+++ test/helpers/headers_test.exs
@@ -2,10 +2,29 @@ defmodule ExJenkins.HeadersTest do
   use ExUnit.Case
 
   describe "extract/2" do
-    test "extract is not case sensitive" do
+    test "not case sensitive lower to upper" do
       headers = [{"header0", 0}, {"header1", 1}]
       name = "HEADER0"
       assert {"header0",0} == ExJenkins.Headers.extract(headers,name)
     end
+
+    test "not case sensitive upper to lower" do
+      headers = [{"HEADER0", 0}, {"HEADER1", 1}]
+      name = "header0"
+      assert {"HEADER0",0} == ExJenkins.Headers.extract(headers,name)
+    end
+
+    test "header not found" do
+      headers = [{"HEADER0", 0}, {"HEADER1", 1}]
+      name = "header3"
+      assert nil == ExJenkins.Headers.extract(headers,name)
+    end
+
+    test "header found and previous ones ignored" do
+      headers = [{"header0", 0}, {"header1", 1}, {"header2", 2}, {"header3", 3}]
+      name = "header3"
+      assert {"header3", 3} == ExJenkins.Headers.extract(headers,name)
+    end
+
   end
 end
