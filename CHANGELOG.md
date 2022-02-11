17453a950adcba2d53eb0813befb61b1b3d79125
Charles Bernasconi <charles.bernasconi@xing.com>
2022-02-11 13:11:11 +0100

    Bump to 0.2.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## v0.2.2
+
+* Bug fixes
+  * Ignore header name in pattern match (#21)
+
 ## v0.2.1
 
 * Bug fixes
--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule ExJenkins.Mixfile do
   def project do
     [
       app: :ex_jenkins,
-      version: "0.2.1",
+      version: "0.2.2",
       elixir: "~> 1.4",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
